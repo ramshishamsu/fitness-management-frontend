a1ab01b223cbfd37685f9cc3aedf7ee6407b0b91
Ramshishamsu <ramshishamsu22@gmail.com>
2026-01-11 13:17:46 +0530

    Remove Vite proxy to fix mixed content

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss(),],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5003", // backend port
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
 });
