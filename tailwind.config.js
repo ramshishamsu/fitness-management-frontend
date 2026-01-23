622470a8db870754108ece1d358cef491029cde3
Ramshishamsu <ramshishamsu22@gmail.com>
2026-01-23 09:27:08 +0530

    Fix Tailwind CSS for production: Use correct PostCSS plugin and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   darkMode: 'class',
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-};
+  plugins: [],
+}
